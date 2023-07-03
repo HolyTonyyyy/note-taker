9029d8318b18f952d3e006de9b16f86827e7d3a8
Anthony Bradford <agb2152001@gmail.com>
2023-07-03 15:10:09 -0400

    commit

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # note-taker
 
-# I tried my best but i dont understand why the notes files is the detected 
+# inside of the db.json requires you to insert square brackets in order to show in the history (not show how to fix that)
+
+# also the delete function does not work 
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const html_routes = require('./routes/html-routes');
-const api_routes = require('./routes/api-routes')
+const html_routes = require('./html-routes');
+const api_routes = require('./api-routes')
 const PORT = process.env.PORT || 3001
 
 const app = express();
@@ -9,8 +9,8 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static('public'));
-app.use(html_routes)
-app.use(api_routes)
+app.use("/",html_routes)
+app.use("/api",api_routes)
 
 app.listen(PORT, () => {
     console.log('Server running on http://localhost:${PORT}')
